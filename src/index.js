9a989465fb9cadb83065dc5b1b941f4b882d58a9
thuongb14 <thuongb14@gmail.com>
2022-10-19 21:20:41 +1100

    bug

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const error = document.querySelector('.error');
+import moment from 'moment';
 
 async function getWeatherData(city) {
   try {
@@ -9,7 +10,7 @@ async function getWeatherData(city) {
     const formatted = await data.json();
     updateData(formatted);
   } catch {
-    error.textContent = 'Oopps...City not found';
+    error.textContent = 'City not found';
   }
 }
 
@@ -19,18 +20,23 @@ function updateData(data) {
   const country = data.sys.country;
   const stat = data.weather[0].main;
   const icon = data.weather[0].icon;
-  updateWeather(celcius, name, country, stat, icon);
+  updateMainWeather(celcius, name, country, stat, icon);
+  updateInfoData(data)
 }
 
-function updateWeather(celcius, name, country, stat, icon) {
+function updateMainWeather(celcius, name, country, stat, icon) {
   const degree = document.querySelector('.degree');
   const location = document.querySelector('.location');
   const weather = document.querySelector('.weather');
   const img = document.querySelector('.icon');
+  const date = document.querySelector('.date')
+  const time = document.querySelector('.time')
   degree.innerHTML = `${celcius}<span>&#176;</span>C`;
   location.textContent = `${name}, ${country}`;
   weather.textContent = stat;
   img.src = `http://openweathermap.org/img/wn/${icon}.png`;
+  date.textContent = `${moment().format('dddd')}, ${moment().format('MMMM Do YYYY')}`
+  time.textContent = moment().format('LT');
 }
 
 const form = document.querySelector('form');
@@ -41,5 +47,21 @@ form.addEventListener('submit', function (e, input) {
   getWeatherData(input);
   form.reset();
 });
+
+function updateInfoData(data) {
+  const humidity = data.main.humidity
+  const feelLike = Math.round(data.main.feels_like - 273.15)
+  const windSpeed = data.wind.speed
+  updateInfoWeather(humidity, feelLike, windSpeed)
+}
+
+function updateInfoWeather(humidity, feelLike, windSpeed) {
+  const humidDegree = document.querySelector('.humidity-degree')
+  const feelLikeDegree = document.querySelector('.feel-like-degree')
+  const windSpeedDegree = document.querySelector('.wind-speed-degree')
+  humidDegree.textContent = `${humidity}%`
+  feelLikeDegree.innerHTML = `${feelLike}&#176;C`
+  windSpeedDegree = `${windSpeed} km/h`
+}
 
 getWeatherData('Melbourne, Au');
